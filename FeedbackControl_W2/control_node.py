5013ba847ab9004f17355798acadabbf6468a9a7
apintorv <andrea.pintorvalencia@gmail.com>
2025-04-04 17:36:54 -0600

    Feedback Control + Euler Final Delivery W2

---
+++ FeedbackControl_W2/control_node.py
@@ -0,0 +1,66 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, Pose
+import numpy as np
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
+
+class Control_Node(Node):
+    def __init__(self):
+        super().__init__('Control_Node')
+        self.get_logger().info("Start control node")
+        
+        qos_profile = QoSProfile(
+            reliability = QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+            history = QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+            depth = 10
+        )
+
+        self.twist = Twist()
+        
+        self.publisher = self.create_publisher(Twist, "/cmd_vel", 10)
+        self.create_subscription(Pose, '/pose', self.position_callback, qos_profile)
+           
+        # Referencias deseadas
+        self.qd = np.array([[0.1, 0.1]]).T
+        self.q0 = np.array([[0.1, 0.1]]).T
+        self.thetha = 0.0
+        
+        # Parámetros del control
+        self.k = 1.0   # Ganancia del controlador
+        self.h = 0.05   # Parámetro de transformación (debe ser diferente de 0)
+        
+        self.timer = self.create_timer(0.001, self.timer_callback)
+        
+    def position_callback(self, msg):
+        self.get_logger().info(f'Actual Position: x:{msg.position.x}, y:{msg.position.y}, z:{msg.orientation.w}')
+        self.q0 = np.array([[msg.position.x, msg.position.y]]).T
+        self.thetha = msg.orientation.w
+        
+        
+    def timer_callback(self):
+        matrix_D = np.array([
+            [np.cos(self.thetha), -self.h * np.sin(self.thetha)],
+            [np.sin(self.thetha),  self.h * np.cos(self.thetha)]
+        ])
+        
+        e = self.q0 - self.qd 
+        aux = -self.k * e
+        
+        U = np.linalg.inv(matrix_D) @ aux
+         
+        self.twist.linear.x = U[0][0]
+        self.twist.angular.z = U[1][0]
+        
+        self.get_logger().info(f'Twist x: {self.twist.linear.x }, Twist z: {self.twist.angular.z}')
+        self.get_logger().info(f'Error:{e}')
+        
+        self.publisher.publish(self.twist)
+
+def main(args=None):
+    rclpy.init(args=args)
+    control = Control_Node()
+    rclpy.spin(control)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ FeedbackControl_W2/position_node.py
@@ -0,0 +1,51 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, Pose
+import numpy as np
+import math
+
+class Position_Node(Node):
+    def __init__(self):
+        super().__init__('Position_Node')
+        self.get_logger().info("Start position node")
+
+        self.subscriber = self.create_subscription(Twist, "/cmd_vel", self.callback, 10)
+        self.velPublisher = self.create_publisher(Pose, "/pose", 10)
+              
+        self.q = np.array([[0, 0, 0]]).T  # Vector de estados q = [x,y,theta]
+        
+        self.tao = 0.001
+        self.h = 0.05
+        
+        self.pose = Pose()
+        
+        self.timer = self.create_timer(0.001, self.timer_callback)
+
+    def timer_callback(self):
+        #self.get_logger().info(f"q: {self.q}")
+        
+        self.pose.position.x = float(self.q[0][0])
+        self.pose.position.y = float(self.q[1][0])
+        self.pose.orientation.w = float(self.q[2][0])
+        
+        self.get_logger().info(f'Actual Position: x:{self.pose.position.x}, y:{self.pose.position.y}, z:{self.pose.orientation.w}')
+        self.velPublisher.publish(self.pose)
+        
+    def callback(self, msg):
+        self.u = np.array([[msg.linear.x, msg.angular.z]]).T
+        self.gx = np.array([
+            [np.cos(self.q[2][0]), -self.h * np.sin(self.q[2][0])],
+            [np.sin(self.q[2][0]), self.h * np.cos(self.q[2][0]) ],
+            [          0,                          1             ]
+        ])
+        
+        self.q = self.q + self.tao * (self.gx @ self.u)  
+        
+def main(args=None):
+    rclpy.init(args=args)
+    position = Position_Node()
+    rclpy.spin(position)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
